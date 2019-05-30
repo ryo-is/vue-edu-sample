98f049857a41f782553865380bfe7134c86469d2
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-30 15:51:12 +0900

    feat:よく使うライフサイクルでconsoleを吐く

--- src/views/newContent/new_content.ts
+++ src/views/newContent/new_content.ts
@@ -1,4 +1,16 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
-export default class NewContent extends Vue {}
+export default class NewContent extends Vue {
+  public created() {
+    console.log("lifecycle Created")
+  }
+
+  public mounted() {
+    console.log("lifecycle Mounted")
+  }
+
+  public updated() {
+    console.log("lifecycle Updated")
+  }
+}
