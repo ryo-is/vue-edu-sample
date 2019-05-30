b9b812cef20e54d032ac2e16e6b1957ebfcd2776
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-30 17:43:32 +0900

    feat:コンテンツを一旦削除する

--- src/views/newContent/new_content.ts
+++ src/views/newContent/new_content.ts
@@ -2,9 +2,6 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class NewContent extends Vue {
-  public text: string = "hogehoge"
-  public array: string[] = ["apple", "banana", "orange"]
-  public textShowFlag: boolean = false
 
   public created() {
     console.log("lifecycle Created")
@@ -16,20 +13,5 @@ export default class NewContent extends Vue {
 
   public updated() {
     console.log("lifecycle Updated")
-  }
-
-  /**
-   * テキストを更新する
-   * @param {String} updateText
-   */
-  public updateText(updateText: string) {
-    this.text = updateText
-  }
-
-  /**
-   * テキストの表示/非表示を切り替える
-   */
-  public showAndHideText() {
-    this.textShowFlag = !this.textShowFlag
   }
 }
